6296b6e460ba19f5156a716124ee4e13d0844229
aryan-pandey2019 <aryan.pandey2019@vitstudent.ac.in>
2022-04-23 04:46:36 +0530

    Update and tnx

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-import json 
+import json
+from datetime import datetime
 import ref_model as ref
+from random import randint as rint
 
 
 with open("info.json", "r") as c:
@@ -22,10 +24,13 @@ login_manager.init_app(app)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(255), nullable = False)
-    password = db.Column(db.String(255), nullable = False)
+    name = db.Column(db.String(512), nullable = False)
+    password = db.Column(db.String(512), nullable = False)
+    account_num = db.Column(db.String(512), nullable = False)
     loan = db.relationship('LoanModel', backref = 'user', lazy = True)
-    user_info = db.relationship('Userinfo', backref='user', lazy = True)
+    user_info = db.relationship('Userinfo', backref = 'user', lazy = True)
+    user_tnx_info = db.relationship('UserTnx', backref = 'user', lazy = True)
+    ## Name, password
 
     def __repr__(self):
         return "Id: " + str(self.id) + "Name: " + self.name + " Email: " + self.email
@@ -38,26 +43,38 @@ class LoanModel(db.Model):
     loan_duration = db.Column(db.Float, nullable = False)
     loan_paid_time = db.Column(db.String(256), nullable = False)
     loan_type = db.Column(db.String(256), nullable = False)
+    date = db.Column(db.DateTime , nullable = False, default = datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    ## loan amt and loan duration
     
-
     def __repr__(self):
         return "Id: " + str(self.id) + "Name: " + str(self.name) + ",Amount: " + str(self.loan_amt)
-    
 
 class Userinfo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     bank_balance = db.Column(db.Float, nullable = False)
     no_of_transactions = db.Column(db.Float, nullable = False)
-    img = db.Column(db.String(255),nullable = False)
-    phn_no = db.Column(db.String(15),nullable = False)
-    father_name = db.Column(db.String(50), nullable = False)
-    mother_name = db.Column(db.String(50), nullable = False)
+    phn_no = db.Column(db.Float,nullable = False)
     address = db.Column(db.Text, nullable = False)
+    date = db.Column(db.DateTime , nullable = False, default = datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    ## img, ph, add
     
     def __repr__(self):
         return "Id: " + str(self.id) + ", User_id" + str(self.user_id)
+    
+class UserTnx(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    date = db.Column(db.DateTime , nullable = False, default = datetime.utcnow)
+    tnx_to = db.Column(db.String(512), nullable = False)
+    tnx_from = db.Column(db.Float, nullable = False)
+    tnx_amt = db.Column(db.Float,nullable = False)
+    tnx_type = db.Column(db.String(512), nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    
+    def __init__(self):
+        return "Id: " + str(self.id) + " User Id: " + str(self.tnx_from)
+    
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -65,7 +82,7 @@ def load_user(user_id):
 
 @app.route('/', methods = ['GET', 'POST'])
 def index_home():
-    return render_template('index.html')
+    return render_template('index2.html')
 
 @app.route('/home', methods = ['GET', 'POST'])
 @login_required
@@ -90,13 +107,15 @@ def login():
 
     return render_template('login.html')
 
+
 @app.route('/signup', methods = ['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         user_name = request.form.get('user_name')
         user_password = request.form.get('user_password')
         user_password = ref.SHA256(user_password)
-        user = User(name = user_name, password = user_password)
+        acct_num = ref.SHA256(str(user_name) + str(user_password))
+        user = User(name = user_name, password = user_password, account_num = acct_num)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('index'))
@@ -110,12 +129,35 @@ def signout():
     logout_user()
     return redirect(url_for('index'))
 
+
+@app.route('/update', methods=['GET' ,'POST'])
+@login_required
+def update():
+    if request.method == 'POST':
+        userinfo = Userinfo(bank_balance = rint(10000, 10000000000), no_of_transactions = 0.0, phn_no = str(request.form.get('phn_no')), address = str(request.form.get('address')), user_id = current_user.id)
+        db.session.add(userinfo)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('update.html', msg = "An error Happended")
+
+@app.route('/maketnx', methods=['GET' ,'POST'])
+@login_required
+def maketnx():
+    if request.method == 'POST':
+        if float(request.form.get('tnx_from')) == current_user.account_num: 
+            tnx_info = UserTnx(tnx_to = request.form.get('tnx_to'),tnx_from = request.form.get('tnx_from'),tnx_amt = float(request.form.get('tnx_amt')),tnx_type = str(request.form.get('tnx_type')),user_id = current_user.id)
+            db.session.add(tnx_info)
+            db.session.commit()
+            return render_template('tnx.html', msg = "Tnx successfully ")
+    return render_template('tnx.html', msg = "An error Happended")
+
 @app.route('/api/info/user', methods = ['GET', 'POST'])
 @login_required
 def api_info_user():
     if request.method == 'GET':
         return jsonify(current_user)
     return("Request Denied")
+
 
 @app.route('/api/info/userinfo', methods = ['GET', 'POST'])
 @login_required
@@ -130,6 +172,7 @@ def api_loan():
     if request.method == 'GET':
         return jsonify(current_user.loan)
     return("Request Denied")
+
 
 
 if __name__ == '__main__':
